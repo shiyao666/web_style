7c07889ced104b473fc35e0cfd12096c442b47ec
shiyao666 <1102699514@qq.com>
2018-10-23 18:28:21 +0800

    ten_10

--- component/card_style/doc/js/set_demo.js
+++ component/card_style/doc/js/set_demo.js
@@ -2,27 +2,32 @@ $(document).ready(function () {
     $('#creat_item_state').bind('click', function () {
         $('#card_page_active').addClass('move-in');
         $('#card_page_active').addClass('active');
+        $('#card_page').addClass('move-out');
+        $('#card_page').removeClass('active');
 
     });
     $('#del_item_state').bind('click', function () {
-        $('#card_page_active').removeClass('active');
+        $('#card_page_active').removeClass('move-in');
+        $('#card_page').removeClass('move-out');
     });
     $('#creat_body_content').bind('click', function () {
-        $('#open_close').addClass('open');
 
+        $('#open_close').addClass('move-out');
+        $('#open_close').addClass('open');
 
     });
     $('#del_body_content').bind('click', function () {
-        $('#open_close').removeClass('open');
-
+        $('#open_close').removeClass('move-out');
     });
+    $('#del_body_reset').bind('click', function () {
+        $('#open_close').removeClass('open');
+    });
+    
     $('#creat_foot_active').bind('click', function () {
         $('#active_modular').addClass('active');
-
 
     });
     $('#del_foot_active').bind('click', function () {
         $('#active_modular').removeClass('active');
-
     });
 })
