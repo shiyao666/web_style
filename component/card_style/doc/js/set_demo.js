9a0beccd9b036bc48649cba03467971419020c7a
shiyao666 <1102699514@qq.com>
2018-10-24 14:57:30 +0800

    ten_10

--- component/card_style/doc/js/set_demo.js
+++ component/card_style/doc/js/set_demo.js
@@ -4,11 +4,16 @@ $(document).ready(function () {
         $('#card_page_active').addClass('active');
         $('#card_page').addClass('move-out');
         $('#card_page').removeClass('active');
-
     });
     $('#del_item_state').bind('click', function () {
         $('#card_page_active').removeClass('move-in');
         $('#card_page').removeClass('move-out');
+    });
+
+    $('#del_item_reset').bind('click', function () {
+        $('#card_page').addClass('active');
+        $('#card_page_active').removeClass('active');
+        $('#card_page_active').removeClass('move-out');
     });
     $('#creat_body_content').bind('click', function () {
 
@@ -22,7 +27,7 @@ $(document).ready(function () {
     $('#del_body_reset').bind('click', function () {
         $('#open_close').removeClass('open');
     });
-    
+
     $('#creat_foot_active').bind('click', function () {
         $('#active_modular').addClass('active');
 
